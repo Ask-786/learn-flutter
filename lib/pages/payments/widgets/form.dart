0a9f7020ea45a1be56ffe00a0c132c0e053f9929
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2023-11-13 23:42:40 +0530

    randome changes

--- lib/pages/payments/widgets/form.dart
+++ lib/pages/payments/widgets/form.dart
@@ -23,121 +23,122 @@ class PaymentsFormWidget extends StatelessWidget {
     return Expanded(
       child: SingleChildScrollView(
         child: Form(
-            key: _formKey,
-            child: Container(
-              margin: const EdgeInsets.all(20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  TextFormField(
-                    controller: _formControllers['amount'],
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Amount'),
-                    validator: (value) => (value == null || value.isEmpty)
-                        ? 'This field is required'
-                        : null,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    controller: _formControllers['description'],
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Description'),
-                    validator: (value) => (value == null || value.isEmpty)
-                        ? 'This field is required'
-                        : null,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    controller: _formControllers['status'],
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Status'),
-                    validator: (value) => (value == null || value.isEmpty)
-                        ? 'This field is required'
-                        : null,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    controller: _formControllers['to'],
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'To'),
-                    validator: (value) => (value == null || value.isEmpty)
-                        ? 'This field is required'
-                        : null,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    controller: _formControllers['date'],
-                    keyboardType: TextInputType.datetime,
-                    onTap: () async {
-                      final date = await openDatePicker(context);
-                      if (date != null) {
-                        _formControllers['date']!.text =
-                            DateFormat('yyyy-MM-dd').format(date);
-                        _formControllers['time']!.text =
-                            DateFormat.jm().format(date);
+          key: _formKey,
+          child: Container(
+            margin: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextFormField(
+                  controller: _formControllers['amount'],
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Amount'),
+                  validator: (value) => (value == null || value.isEmpty)
+                      ? 'This field is required'
+                      : null,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _formControllers['description'],
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Description'),
+                  validator: (value) => (value == null || value.isEmpty)
+                      ? 'This field is required'
+                      : null,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _formControllers['status'],
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Status'),
+                  validator: (value) => (value == null || value.isEmpty)
+                      ? 'This field is required'
+                      : null,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _formControllers['to'],
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'To'),
+                  validator: (value) => (value == null || value.isEmpty)
+                      ? 'This field is required'
+                      : null,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _formControllers['date'],
+                  keyboardType: TextInputType.datetime,
+                  onTap: () async {
+                    final date = await openDatePicker(context);
+                    if (date != null) {
+                      _formControllers['date']!.text =
+                          DateFormat('yyyy-MM-dd').format(date);
+                      _formControllers['time']!.text =
+                          DateFormat.jm().format(date);
+                    }
+                  },
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Date'),
+                  validator: (value) => (value == null || value.isEmpty)
+                      ? 'This field is required'
+                      : null,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _formControllers['type'],
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Type'),
+                  validator: (value) => (value == null || value.isEmpty)
+                      ? 'This field is required'
+                      : null,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _formControllers['from'],
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'From'),
+                  validator: (value) => (value == null || value.isEmpty)
+                      ? 'This field is required'
+                      : null,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _formControllers['source'],
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Source'),
+                  validator: (value) => (value == null || value.isEmpty)
+                      ? 'This field is required'
+                      : null,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.pushNamed(
+                            context, '/payments/payments-details',
+                            arguments: _formControllers);
                       }
                     },
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Date'),
-                    validator: (value) => (value == null || value.isEmpty)
-                        ? 'This field is required'
-                        : null,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    controller: _formControllers['type'],
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Type'),
-                    validator: (value) => (value == null || value.isEmpty)
-                        ? 'This field is required'
-                        : null,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    controller: _formControllers['from'],
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'From'),
-                    validator: (value) => (value == null || value.isEmpty)
-                        ? 'This field is required'
-                        : null,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    controller: _formControllers['source'],
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Source'),
-                    validator: (value) => (value == null || value.isEmpty)
-                        ? 'This field is required'
-                        : null,
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          Navigator.pushNamed(
-                              context, '/payments/payments-details',
-                              arguments: _formControllers);
-                        }
-                      },
-                      child: const Text('Submit')),
-                ],
-              ),
-            )),
+                    child: const Text('Submit')),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
